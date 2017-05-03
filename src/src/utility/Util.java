e04943904fc20fee48d16ba89394b18426c60ee9
BSSwanney <quigson7@yahoo.com>
2017-05-03 14:30:19 -0500

    Add files via upload

---
+++ src/src/utility/Util.java
@@ -0,0 +1,30 @@
+package utility;
+
+import java.awt.GraphicsEnvironment;
+import java.io.InputStream;
+import java.net.URL;
+
+public class Util {
+	
+	//returns the system time in milliseconds
+	public static long getTime(){
+		return System.currentTimeMillis();
+	}
+	
+	//returns the width and height of the screen
+	public static int getScreenWidth(){
+		return GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice().getDisplayMode().getWidth();
+	}
+	public static int getScreenHeight(){
+		return GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice().getDisplayMode().getHeight();
+	}
+	
+	//returns the stream for the resource
+	public static InputStream getFileStream(String fileName){
+		return Util.class.getResourceAsStream(fileName);
+	}
+	//returns the url for the resource
+	public static URL getFileURL(String fileName){
+		return Util.class.getClassLoader().getResource(fileName);
+	}
+}
