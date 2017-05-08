90b29a51f7d2684028f0016db16d86309b28885c
Tmanpenguin <taylor_schorlemmer@yahoo.com>
2017-05-08 14:30:04 -0500

    Add files via upload

--- src/gameObjects/Character.java
+++ src/gameObjects/Character.java
@@ -6,31 +6,32 @@ public class Character extends Entity {
 
 	private double angle;
 	
-	private double xVelocity;
+	private double xVelocity;//2d velocities of the character
 	private double yVelocity;
-	private double maxVelocity;
-	private double friction;
-	private double acceleration;
-	private boolean accX, accY;
+	private double maxVelocity;//maximum velocity of character in x and y direction
+	private double friction;//defines how quickly the object stops moving ***LOWER THE VALUE - GREATER THE FRICTION
+	private double acceleration;//defines how quickly an object starts moving
+	private boolean moveUp, moveDown, moveLeft, moveRight;
 	
 	public Character(double x, double y, Image sprites) {
 		super(x, y, sprites);
-		this.maxVelocity = 5;
-		this.friction = .9;
-		this.setAcceleration(1);
-		this.accX = false;
-		this.accY = false;
+		setDefaultMovement();
 	}
 	public Character(Image sprites){
 		super(sprites);
-		this.maxVelocity = 5;
-		this.friction = .9;
-		this.setAcceleration(1);
-		this.accX = false;
-		this.accY = false;
+		setDefaultMovement();
 	}
 	
-	
+	private void setDefaultMovement(){//default movement values, can be changed through getters and setters
+		this.maxVelocity = 20;
+		this.friction = .8;
+		this.setAcceleration(1);
+		this.moveUp = false;
+		this.moveDown = false;
+		this.moveLeft = false;
+		this.moveRight = false;
+	}
+
 	public double getAngle() {
 		return angle;
 	}
@@ -63,23 +64,52 @@ public class Character extends Entity {
 	}
 	
 	public void updateLocation(){
-		if(xVelocity > maxVelocity)
+		
+		if(moveRight && moveLeft)//check for horizontal movement cancellation ie - user going left and right at same time
+			xVelocity *= friction;
+		else if(moveRight){//if moving right
+				if(acceleration > -(xVelocity-xVelocity*friction))//check to see if friction or acceleration will change velocity quicker
+					xVelocity += acceleration;//adds to velocity
+				else
+					xVelocity *= friction;//use friction
+			}
+		else if(moveLeft){//if moving left, uses same concepts as above
+			if(acceleration > (xVelocity-xVelocity*friction))
+				xVelocity -= acceleration;
+			else
+				xVelocity *= friction;
+		}
+		else{//if not moving
+			xVelocity *= friction;
+		}
+		
+		
+		if(moveUp && moveDown)//check for vert. movement cancellation
+			yVelocity *= friction;	
+		else if(moveUp){//if moving up
+			if(acceleration > (yVelocity-yVelocity*friction))
+				yVelocity -= acceleration;
+			else
+				yVelocity *= friction;
+		}
+		else if(moveDown){//if moving down
+			if(acceleration > -(yVelocity-yVelocity*friction))
+				yVelocity += acceleration;
+			else
+				yVelocity *= friction;
+		}
+		else{//if not moving
+			yVelocity *= friction;
+		}
+		
+		
+		if(xVelocity > maxVelocity)//check to see if object is moving too fast
 			xVelocity = maxVelocity;
 		if(yVelocity > maxVelocity)
 			yVelocity = maxVelocity;
-		this.addX(xVelocity);
+		
+		this.addX(xVelocity);//moving the object by adding velocity values to the x and y position
 		this.addY(yVelocity);
-		
-		if(accX)
-			xVelocity += acceleration;
-		else
-			xVelocity *= friction;
-		if(accY)
-			yVelocity += acceleration;
-		else
-			yVelocity *= friction;
-		
-		
 	}
 	
 	public double getAcceleration() {
@@ -87,6 +117,30 @@ public class Character extends Entity {
 	}
 	public void setAcceleration(double acceleration) {
 		this.acceleration = acceleration;
+	}
+	public boolean isMoveUp() {
+		return moveUp;
+	}
+	public void setMoveUp(boolean moveUp) {
+		this.moveUp = moveUp;
+	}
+	public boolean isMoveDown() {
+		return moveDown;
+	}
+	public void setMoveDown(boolean moveDown) {
+		this.moveDown = moveDown;
+	}
+	public boolean isMoveLeft() {
+		return moveLeft;
+	}
+	public void setMoveLeft(boolean moveLeft) {
+		this.moveLeft = moveLeft;
+	}
+	public boolean isMoveRight() {
+		return moveRight;
+	}
+	public void setMoveRight(boolean moveRight) {
+		this.moveRight = moveRight;
 	}
 	
 }
